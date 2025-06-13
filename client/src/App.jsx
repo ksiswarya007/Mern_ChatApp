2cdf0f94e84b3c40df1285baa819b07d0b90cad8
ksiswarya007 <136336216+ksiswarya007@users.noreply.github.com>
2025-06-13 14:27:56 +0530

    updated frontend and backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   }, [authUser]);
 
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster/>
       <Routes>
 
--- server/server.js
+++ server/server.js
@@ -46,6 +46,10 @@ app.use("/api/messages",messageRouter);
 //connect to mongoDB
 await connectDB();
 
+if(process.env.NODE_ENV!=="production"){
 const PORT=process.env.PORT ||5000;
 server.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
+}
 
+//Export server for vercel
+export default server;
